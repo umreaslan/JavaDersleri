af24d153b2d5ca301bfaf055575a4bc063afaed2
unknown <sevdeneda.38@gmail.com>
2022-12-30 16:13:44 +0300

    30.12.22

---
+++ src/day01programlamayaGırıs/C01_HelloWorld.java
@@ -0,0 +1,15 @@
+package day01programlamayaGırıs;
+
+public class C01_HelloWorld {
+
+    public static void main(String[] args) {
+     // prıntln yazdırdıgımız zaman  alt satıra gecer.sadece prınt yazarsak alt satıra gecmez.
+        //aynı satırdan devam eder
+
+        System.out.println("Hello World");
+
+        System.out.println("Bugun kodlamada ılk gunumuz");
+
+        System.out.println("bu da 3.satır olsun");
+    }
+}
---
+++ src/day01programlamayaGırıs/C02_varıables.java
@@ -0,0 +1,17 @@
+package day01programlamayaGırıs;
+
+public class C02_varıables {
+    public static void main(String[] args) {
+    //
+
+        int sayı=20;
+        System.out.println(sayı);//sayı varıablenın degerını yazdırır
+        System.out.println("sayı");//sayı
+
+        sayı=30;
+        System.out.println(sayı);
+
+        sayı=sayı+20;
+        System.out.println(sayı);
+    }
+}
